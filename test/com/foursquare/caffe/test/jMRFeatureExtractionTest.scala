bf80e11788964fa8658d927312c3026492016c0a
Zengpan Fan <zen@foursquare.com>
2016-08-08 12:43:47 +0000

    Change Scala test case

--- test/com/foursquare/caffe/test/jMRFeatureExtractionTest.scala
+++ test/com/foursquare/caffe/test/jMRFeatureExtractionTest.scala
@@ -6,22 +6,35 @@ import caffe.Caffe.Datum
 import collection.JavaConverters._
 import com.foursquare.caffe.jMRFeatureExtraction
 import com.google.protobuf.ByteString
-import java.io.{File, FileInputStream, FileOutputStream}
 import java.awt.image.{BufferedImage, DataBufferByte}
+import java.io.{File, FileInputStream, FileOutputStream}
+import java.lang.{Runnable, Thread}
 import javax.imageio.ImageIO
 import scala.io.Source
 
 object jMRFeatureExtractionTestApp extends App {
   val featureExtraction = new jMRFeatureExtraction
-
-  featureExtraction.start(args(1), args(2))
-
   val fileListPath = args(0)
   val source = Source.fromFile(fileListPath)
   val fileList = source.getLines
+  val resultWriter = new PrintWriter(args.lift(1).getOrElse("jMRFeatureExtractionTestApp_result.txt"))
 
-  (0 until 51).foreach(idx => {
-    val f = fileList.next.split(' ')(0)
+  featureExtraction.start(
+    args.lift(2).getOrElse("bvlc_googlenet.caffemodel"),
+    args.lift(3).getOrElse("train_val.prototxt")
+  )
+
+  val worker = new Thread(new Runnable() {
+    void run() {
+      val featureDatum = featureExtraction.readDatum()
+
+      featureDarum.getFloatDataList.asScala.foreach(d => resultWriter.println(s"$d "))
+    }
+  })
+
+  worker.start()
+
+  fileList.foreach(f => {
     val img = ImageIO.read(new File(f))
     val byteArray = img.getRaster.getDataBuffer.asInstanceOf[DataBufferByte].getData
     val datum = Datum.newBuilder
@@ -34,17 +47,16 @@ object jMRFeatureExtractionTestApp extends App {
     featureExtraction.writeDatum(datum)
   })
 
-  source.close
-
   // TODO(zen): add flush method to force sync.
   val ret = featureExtraction.stop()
 
-  val featureDatum = featureExtraction.readDatum()
-
-  featureDatum.getFloatDataList.asScala.foreach(d => println(s"$d "))
-
   println(ret)
 
-  inputStream.close()
-  outputStream.close()
+  worker.interrupt()
+
+  resultWriter.close
+  source.close
+
+  inputStream.close
+  outputStream.close
 }
